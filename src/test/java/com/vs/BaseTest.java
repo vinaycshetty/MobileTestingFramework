fa1fe75b9ba6eb5e942130282290648423ab0d3f
Vinay Shetty <shettyvinayc@gmail.com>
2025-03-10 09:50:05 +0530

    Long Press Gesture test

--- src/test/java/com/vs/BaseTest.java
+++ src/test/java/com/vs/BaseTest.java
@@ -1,11 +1,17 @@
 package com.vs;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -24,9 +30,9 @@ public class BaseTest {
         // Appium code -> appium server -> mobile
         // to start appium programmatically
         // file path
-        /**  File file = new File("C://Users//vinay//AppData//Roaming//npm//node_modules//appium//build//lib//main.js");
-         service = new AppiumServiceBuilder().withAppiumJS(file).withIPAddress("127.0.0.1").usingPort(4729).build();
-         serviceBuilder.start(); */
+//        File file = new File("C://Users//vinay//AppData//Roaming//npm//node_modules//appium//build//lib//main.js");
+//         service = new AppiumServiceBuilder().withAppiumJS(file).withIPAddress("127.0.0.1").usingPort(4723).build();
+//        service.start();
         // two parameters to pass is one URL to appium server and options
         URL url = new URL("http://127.0.0.1:4723");
         // options
@@ -37,9 +43,13 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    public void LongPressAction(WebElement element){
+        ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture", ImmutableMap.of("elementId",((RemoteWebElement)element).getId(),"duration",2000));
+    }
+
     @AfterClass
     public void tearDown() {
         driver.quit();
-//        serviceBuilder.stop();
+//        service.stop();
     }
 }
---
+++ src/test/java/com/vs/LongPressExample.java
@@ -0,0 +1,29 @@
+package com.vs;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by : Vinay Shetty
+ * on 10-03-2025 at 09:30
+ **/
+public class LongPressExample extends BaseTest {
+    @Test
+    public void LongPressTest(){
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Expandable Lists']")).click();
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+        WebElement element = driver.findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
+        LongPressAction(element);
+        String menuText = driver.findElement(By.id("android:id/title")).getText();
+        Assert.assertEquals(menuText,"Sample menu");
+        driver.findElement(By.id("android:id/title")).isDisplayed();
+
+    }
+}
